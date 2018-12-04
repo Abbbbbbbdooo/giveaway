3a983ff6a07bc63d5f1238a098c06618b3eed7cc
Abdo <45042294+Abbbbbbbdooo@users.noreply.github.com>
2018-12-04 17:28:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 var prefix = "*"
+
 const moment = require('moment');
 client.on('message',async message => {
   var time = moment().format('Do MMMM YYYY , hh:mm');
